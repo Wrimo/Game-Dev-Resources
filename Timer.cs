99ef4f3912608e97b31b5d6de8b2fbc744a2dc74
Wrimo <38633801+Wrimo@users.noreply.github.com>
2019-07-11 17:35:55 -0400

    Add files via upload

---
+++ Timer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer
+{
+    public int CountTo;
+    public int StartCount;
+    public int Increment;
+    public int CurrentCount;
+
+    public Timer()
+    {
+        CountTo = 60;
+        StartCount = 0;
+        Increment = 1;
+        CurrentCount = 0; 
+    }
+    public Timer(int countTo, int startCount, int increment, int currentCount)
+    {
+        CountTo = countTo;
+        StartCount = startCount;
+        Increment = increment;
+        CurrentCount = currentCount;
+    }
+
+    public bool CheckTimer()
+    {
+        if(CurrentCount >= CountTo)
+        {
+            return true; 
+        } 
+        else
+        {
+            return false;
+        }
+    }
+
+    public void ResetTimer()
+    {
+        CurrentCount = 0; 
+    }
+}
